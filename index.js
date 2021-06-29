9344bf2bdf6bf20cba8c047d4bd3a35250cd7857
amsalinas03 <73053309+amsalinas03@users.noreply.github.com>
2021-06-29 13:01:11 -0500

    Create index.js
    also added main menu options

---
+++ index.js
@@ -0,0 +1,42 @@
+const inquirer = require("inquirer");
+
+function mainMenu() {
+    console.log("Welcome to the Employee Tracker System")
+    inquirer.prompt(
+        {
+        type: "list",
+        name: "mainMenuChoices",
+        message: "Please choose from the following options",
+        choices: [
+            {
+                name: "View Employees",
+                value: "viewEmployees"
+            },
+            {
+                name: "View Employees by Department",
+                value: "viewEmployeesByDepartment"
+            },
+            {
+                name: "View Employees By Manager",
+                value: "viewEmployeesByManager"
+            },
+            {
+                name: "More",
+                value: "moreMainMenu"
+            }
+        ]
+        }.then((choice) => {
+            switch(choice) {
+                case "viewEmployees":
+                    return viewEmployees();
+                case "viewEmployeesByDepartment":
+                    return viewEmployeesByDepartment();
+                case "viewEmployeesByManager":
+                    return viewEmployeesByManager();
+                case "More":
+                    return moreMainMenu();
+            }
+        }
+        )
+    )
+}
