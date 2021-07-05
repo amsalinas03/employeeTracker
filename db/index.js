999a5f278cd20031400fa397ac711906000b9e78
amsalinas03 <73053309+amsalinas03@users.noreply.github.com>
2021-07-04 23:30:09 -0500

    created function to view departments

--- db/index.js
+++ db/index.js
@@ -18,6 +18,12 @@ function addDepartment() {
         "INSERT INTO department SET ?"
     )
 }
+
+function viewDepartments() {
+    return connection.query(
+        "SELECT department.id, department.name, SUM(role.salary) AS utilized_budget FROM department LEFT JOIN role ON role.department_id = department.id LEFT JOIN employee ON employee.role_id = role.id GROUP BY department.id, department.name"
+    )
+}
 function viewAllEmployees() {
     return connection.query(
         "SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager FROM employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN department on role.department_id = department.id LEFT JOIN employee manager on manager.id = employee.manager_id;"
