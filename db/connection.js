68114e9e9ebe482d670b735c24a15335d985b49b
amsalinas03 <73053309+amsalinas03@users.noreply.github.com>
2021-07-05 00:29:51 -0500

    Update connection.js
    matched database name to schema

--- db/connection.js
+++ db/connection.js
@@ -4,7 +4,7 @@ const connection = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "whippersnapper3",
-    database: "employees"
+    database: "employees_db"
   });
   
   connection.connect();
