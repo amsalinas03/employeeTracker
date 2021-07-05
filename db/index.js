655be40231aeb7736c6b96680b00001b87a6f6c9
amsalinas03 <73053309+amsalinas03@users.noreply.github.com>
2021-07-04 23:31:20 -0500

    created function to view roles

--- db/index.js
+++ db/index.js
@@ -24,6 +24,11 @@ function viewDepartments() {
         "SELECT department.id, department.name, SUM(role.salary) AS utilized_budget FROM department LEFT JOIN role ON role.department_id = department.id LEFT JOIN employee ON employee.role_id = role.id GROUP BY department.id, department.name"
     )
 }
+function viewRoles() {
+    return connection.query(
+        "SELECT role.id, role.title, department.name AS department, role.salary FROM role LEFT JOIN department on role.department_id = department.id"
+    )
+}
 function viewAllEmployees() {
     return connection.query(
         "SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager FROM employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN department on role.department_id = department.id LEFT JOIN employee manager on manager.id = employee.manager_id;"
